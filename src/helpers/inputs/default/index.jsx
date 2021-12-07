91efcd5c487826982c41bb0eb7946293b52cde0b
Hector Rodrigues da Silva <hectddos103@gmail.com>
2021-12-07 18:31:23 -0300

    adicionando nova propriedade type nos detailsInput no helper InputDefault

--- src/helpers/inputs/default/index.jsx
+++ src/helpers/inputs/default/index.jsx
@@ -6,6 +6,7 @@ import React, { Fragment } from 'react';
 export const InputDefault = React.memo((props) => {
     const detailsInput = {
         size: props.size ?? 'md',
+        type: props.type ?? 'text',
         error: props.error ? ` is-invalid` : '',
         className: props.className ? ` ${props.className}` : ''
     };
@@ -13,7 +14,7 @@ export const InputDefault = React.memo((props) => {
         <Fragment>
             <Input
                 {...props}
-                type={props.type ?? 'text'}
+                type={detailsInput.type}
                 className={`form-control form-control-${detailsInput.size}${detailsInput.className}${detailsInput.error}`}
             />
             <Invalid>
