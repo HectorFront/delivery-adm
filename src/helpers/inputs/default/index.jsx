65c04381e2a831716845f65c1c1e73880ca12ec2
Hector Rodrigues da Silva <hectddos103@gmail.com>
2021-12-07 18:29:48 -0300

    melhoria no helper de input

--- src/helpers/inputs/default/index.jsx
+++ src/helpers/inputs/default/index.jsx
@@ -3,15 +3,22 @@ import { Input, Invalid } from "./styled";
 /** @name Dependencies */
 import React, { Fragment } from 'react';
 
-export const InputDefault = React.memo((props) =>
-    <Fragment>
-        <Input
-            {...props}
-            type={props.type ?? 'text'}
-            className={`form-control form-control-${props.size??'md'}${props.className?` ${props.className}`:''}${props.error ?' is-invalid':''}`}
-        />
-        <Invalid>
-            Preencha o campo corretamente.
-        </Invalid>
-    </Fragment>
-);
+export const InputDefault = React.memo((props) => {
+    const detailsInput = {
+        size: props.size ?? 'md',
+        error: props.error ? ` is-invalid` : '',
+        className: props.className ? ` ${props.className}` : ''
+    };
+    return (
+        <Fragment>
+            <Input
+                {...props}
+                type={props.type ?? 'text'}
+                className={`form-control form-control-${detailsInput.size}${detailsInput.className}${detailsInput.error}`}
+            />
+            <Invalid>
+                Preencha o campo corretamente.
+            </Invalid>
+        </Fragment>
+    );
+});
