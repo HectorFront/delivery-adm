0165af6090f4d65892266589c8f89db23868b703
Hector Silva <55253700+HectorFront@users.noreply.github.com>
2022-04-21 09:58:04 -0300

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -19,3 +19,8 @@ import reportWebVitals from './reportWebVitals';
 dotenv.config({ path: '../.env' });
 
 ReactDOM.render(<Routes/>, document.getElementById('root'));
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
