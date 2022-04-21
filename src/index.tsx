bbae6a6197fdeed4c88a738a7fb2dedf50ca59b6
Hector Silva <55253700+HectorFront@users.noreply.github.com>
2022-04-21 09:56:58 -0300

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -14,6 +14,8 @@ import dotenv from 'dotenv';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Routes from './routes';
+import reportWebVitals from './reportWebVitals';
+
 dotenv.config({ path: '../.env' });
 
 ReactDOM.render(<Routes/>, document.getElementById('root'));
